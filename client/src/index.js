5d711605e1e90b9ceb907701c7a06aed9d295629
agosfrixione <agosfrixione@gmail.com>
2022-11-16 19:47:42 -0300

    cambio URL

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios'
 
 // axios.defaults.baseURL = "http://localhost:3001";
-axios.defaults.baseURL = "https://pi-videogames.up.railway.app/";
+axios.defaults.baseURL = "https://pi-videogames-back.up.railway.app/";
 
 ReactDOM.render(
   <React.StrictMode>
